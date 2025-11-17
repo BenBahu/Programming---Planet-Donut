e8e4f0a7606c5bb341cec4b395b955cb415ad9dc
Benjamin Bahurel <126111784+BenBahu@users.noreply.github.com>
2025-11-17 14:30:17 +0100

    t

--- simulation.h
+++ simulation.h
@@ -1,5 +1,5 @@
 // simulation
-// Auteurs : Benjamin Bahurel et Daniel Silva
+// Auteurs : Benjamin Bahurel et Matthieu Lavoix
 
 
 #ifndef SIMULATION
@@ -77,6 +77,7 @@ class Simulation{
 	
 	
 #endif
+
 
 
 
