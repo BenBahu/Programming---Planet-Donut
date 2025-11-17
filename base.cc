bce23b048d8147d642e03f6d3aaea5c76842ed35
Benjamin Bahurel <126111784+BenBahu@users.noreply.github.com>
2025-11-17 14:23:40 +0100

    text

--- base.cc
+++ base.cc
@@ -1,5 +1,5 @@
 // base.cc
-// Auteurs : Georg Schwabedal et Daniel Silva
+// Auteurs : Benjamin Bahurel et Daniel Silva
 
 #include "base.h"
 #include "constantes.h"
@@ -621,6 +621,7 @@ void Base::rec_DEF( shared_ptr <Base> B ,  shared_ptr <Robot> A )
 		}				
 	}
 }
+
 
 
 
